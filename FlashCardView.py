644301c41e26eb0b505ab3684a48bdb85c3b343a
flavio <fdanesse@gmail.com>
2014-12-03 22:32:39 -0200

    Vista de FlashCards

--- FlashCardView.py
+++ FlashCardView.py
@@ -20,6 +20,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import gtk
+import pango
 
 from Globales import COLORES
 
@@ -30,12 +31,26 @@ class FlashCardView(gtk.EventBox):
 
         gtk.EventBox.__init__(self)
 
-        self.topic = False
-
         self.modify_bg(gtk.STATE_NORMAL, COLORES["toolbar"])
         self.set_border_width(4)
 
-        self.add(gtk.Label("FlashCards"))
+        self.topic = False
+
+        tabla = gtk.Table(rows=10, columns=5, homogeneous=True)
+        tabla.set_property("column-spacing", 5)
+        tabla.set_property("row-spacing", 5)
+        tabla.set_border_width(4)
+
+        cabecera = Cabecera()
+        flashcard = FlashCard()
+
+        tabla.attach(cabecera, 0, 3, 0, 2)
+        tabla.attach(flashcard, 0, 3, 2, 10)
+
+        derecha = Derecha()
+        tabla.attach(derecha, 3, 5, 2, 10)
+
+        self.add(tabla)
         self.show_all()
 
     def stop(self):
@@ -44,3 +59,87 @@ class FlashCardView(gtk.EventBox):
     def run(self, topic):
         self.topic = topic
         self.show()
+
+
+class FlashCard(gtk.EventBox):
+
+    def __init__(self):
+
+        gtk.EventBox.__init__(self)
+
+        self.modify_bg(gtk.STATE_NORMAL, COLORES["toolbar"])
+        self.set_border_width(10)
+
+        self.add(gtk.DrawingArea())
+        self.show_all()
+
+
+class Cabecera(gtk.EventBox):
+
+    def __init__(self):
+
+        gtk.EventBox.__init__(self)
+
+        self.modify_bg(gtk.STATE_NORMAL, COLORES["toolbar"])
+
+        tabla = gtk.Table(rows=2, columns=2, homogeneous=True)
+        tabla.set_property("column-spacing", 5)
+        tabla.set_property("row-spacing", 5)
+        tabla.set_border_width(4)
+
+        self.titulo = gtk.Label("Título")
+        label1 = gtk.Label("Keywords")
+        label2 = gtk.Label("What is This?")
+
+        tabla.attach(self.titulo, 0, 2, 0, 1)
+        tabla.attach(label1, 0, 1, 1, 2)
+        tabla.attach(label2, 1, 2, 1, 2)
+
+        self.add(tabla)
+        self.show_all()
+
+
+class Derecha(gtk.EventBox):
+
+    def __init__(self):
+
+        gtk.EventBox.__init__(self)
+
+        self.modify_bg(gtk.STATE_NORMAL, COLORES["toolbar"])
+
+        tabla = gtk.Table(rows=4, columns=3, homogeneous=True)
+        tabla.set_property("column-spacing", 5)
+        tabla.set_property("row-spacing", 5)
+        tabla.set_border_width(4)
+
+        button0 = MyButton("Show me the answer",
+            pango.FontDescription("Purisa 12"))
+        tabla.attach(button0, 0, 3, 1, 2)
+
+        button1 = MyButton("Had not\nidea",
+            pango.FontDescription("Purisa 8"))
+        tabla.attach(button1, 0, 1, 2, 3)
+
+        button1 = MyButton("Just What\nThougth",
+            pango.FontDescription("Purisa 8"))
+        tabla.attach(button1, 1, 2, 2, 3)
+
+        button1 = MyButton("Thew it !",
+            pango.FontDescription("Purisa 8"))
+        tabla.attach(button1, 2, 3, 2, 3)
+
+        self.add(tabla)
+        self.show_all()
+
+
+class MyButton(gtk.Button):
+
+    def __init__(self, text, font):
+
+        gtk.Button.__init__(self)
+
+        label = gtk.Label(text)
+        label.set_property("justify", gtk.JUSTIFY_CENTER)
+        label.modify_font(font)
+        self.set_property("child", label)
+        self.show_all()
