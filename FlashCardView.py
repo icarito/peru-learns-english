3bed9fbeee6fd00da244411b95e748317c620332
flavio <fdanesse@gmail.com>
2014-12-04 01:00:54 -0200

    Funcionalidad de Botonera y Visor de Imágenes de FlashCards

--- FlashCardView.py
+++ FlashCardView.py
@@ -24,6 +24,7 @@ import gtk
 import pango
 import gobject
 
+from JAMediaImagenes.ImagePlayer import ImagePlayer
 from Globales import COLORES
 from Globales import get_vocabulario
 
@@ -39,6 +40,8 @@ class FlashCardView(gtk.EventBox):
 
         self.topic = False
         self.vocabulario = []
+        self.imagenplayer = False
+        self.index_select = 1
 
         tabla = gtk.Table(rows=10, columns=5, homogeneous=True)
         tabla.set_property("column-spacing", 5)
@@ -46,10 +49,10 @@ class FlashCardView(gtk.EventBox):
         tabla.set_border_width(4)
 
         cabecera = Cabecera()
-        flashcard = FlashCard()
+        self.flashcard = FlashCard()
 
         tabla.attach(cabecera, 0, 3, 0, 2)
-        tabla.attach(flashcard, 0, 3, 2, 10)
+        tabla.attach(self.flashcard, 0, 3, 2, 10)
 
         self.derecha = Derecha()
         tabla.attach(self.derecha, 3, 5, 2, 10)
@@ -57,20 +60,65 @@ class FlashCardView(gtk.EventBox):
         self.add(tabla)
         self.show_all()
 
+        self.derecha.connect("siguiente", self.__siguiente)
+        self.derecha.connect("show_answer", self.__show_answer)
+
+    def __show_answer(self, widget):
+        # FIXME: Pronunciar Respuesta
+        # FIXME: Mostrar Respuesta
+        pass
+
+    def __siguiente(self, widget, respuesta):
+        """
+        Continúa con siguiente palabra del bocabulario cargado.
+        """
+        # FIXME: Persistir datos según respuesta y self.vocabulario[self.index_select]
+        # FIXME: Tomar indice según persistencia
+        if self.index_select < len(self.vocabulario) - 1:
+            self.index_select += 1
+        else:
+            self.index_select = 1
+        gobject.timeout_add(500, self.__load, self.index_select)
+
+    def __load(self, index):
+        """
+        Carga una nueva palabra del Bocabulario
+        """
+        path = os.path.join(self.topic, "Imagenes",
+            "%s.png" % self.vocabulario[index][0])
+        if self.imagenplayer:
+            self.imagenplayer.stop()
+            del(self.imagenplayer)
+            self.imagenplayer = False
+        self.imagenplayer = ImagePlayer(self.flashcard.drawing)
+        self.imagenplayer.load(path)
+        # FIXME: Decir "What is This?" + efectos gráficos requeridos
+        gobject.timeout_add(500, self.derecha.activar)
+        return False
+
     def stop(self):
+        """
+        Desactiva la vista de FlashCards.
+        """
         self.hide()
+        if self.imagenplayer:
+            self.imagenplayer.stop()
+            del(self.imagenplayer)
+            self.imagenplayer = False
 
     def run(self, topic):
+        """
+        Carga Vocabulario, pone widgets a estado inicial y
+        carga primera palabra.
+        """
         self.derecha.run()
         self.topic = topic
         csvfile = os.path.join(topic, "vocabulario.csv")
         self.vocabulario = get_vocabulario(csvfile)
         self.show()
-        gobject.timeout_add(500, self.__load, 1)
-
-    def __load(self, index):
-        print self.vocabulario[index]
-        return False
+        # FIXME: Tomar indice según persistencia
+        self.index_select = 1
+        gobject.timeout_add(500, self.__load, self.index_select)
 
 
 class FlashCard(gtk.EventBox):
@@ -116,6 +164,12 @@ class Cabecera(gtk.EventBox):
 
 class Derecha(gtk.EventBox):
 
+    __gsignals__ = {
+    "siguiente": (gobject.SIGNAL_RUN_FIRST,
+        gobject.TYPE_NONE, (gobject.TYPE_INT, )),
+    "show_answer": (gobject.SIGNAL_RUN_FIRST,
+        gobject.TYPE_NONE, [])}
+
     def __init__(self):
 
         gtk.EventBox.__init__(self)
@@ -129,18 +183,22 @@ class Derecha(gtk.EventBox):
 
         button0 = MyButton("Show me the answer",
             pango.FontDescription("Purisa 12"))
+        button0.connect("clicked", self.__show_answer)
         tabla.attach(button0, 0, 3, 1, 2)
 
         button1 = MyButton("Had not\nidea",
             pango.FontDescription("Purisa 8"))
+        button1.connect("clicked", self.__seguir)
         tabla.attach(button1, 0, 1, 2, 3)
 
         button2 = MyButton("Just What\nThougth",
             pango.FontDescription("Purisa 8"))
+        button2.connect("clicked", self.__seguir)
         tabla.attach(button2, 1, 2, 2, 3)
 
         button3 = MyButton("Thew it !",
             pango.FontDescription("Purisa 8"))
+        button3.connect("clicked", self.__seguir)
         tabla.attach(button3, 2, 3, 2, 3)
 
         self.buttons = [button0, button1, button2, button3]
@@ -148,11 +206,24 @@ class Derecha(gtk.EventBox):
         self.add(tabla)
         self.show_all()
 
+    def __seguir(self, button):
+        self.run()
+        self.emit("siguiente", self.buttons.index(button))
+
+    def __show_answer(self, button):
+        self.emit("show_answer")
+        self.buttons[1].show()
+        self.buttons[2].show()
+        self.buttons[3].show()
+
     def run(self):
         self.buttons[0].set_sensitive(False)
         self.buttons[1].hide()
         self.buttons[2].hide()
         self.buttons[3].hide()
+
+    def activar(self):
+        self.buttons[0].set_sensitive(True)
 
 
 class MyButton(gtk.Button):
---
+++ JAMediaImagenes/ImagePlayer.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   ImagePlayer.py por:
+#   Flavio Danesse <fdanesse@gmail.com>
+#   Uruguay
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+Descripción:
+    Visor de Imágenes en base a gstreamer.
+
+    Recibe un widget gtk para dibujar sobre él.
+
+    Utilice la función: load(file_path)
+        para cargar el archivo a dibujar.
+
+    Utilice la función: stop()
+        para detener la reproducción
+
+    Utilice la función: rotar("Derecha") o rotar("Izquierda")
+        para rotar la imágen
+"""
+
+import os
+import gobject
+import gst
+import gtk
+
+PR = False
+
+
+class ImagePlayer(gobject.GObject):
+
+    def __init__(self, ventana):
+
+        gobject.GObject.__init__(self)
+
+        self.ventana = ventana
+        self.src_path = ""
+
+        rect = self.ventana.get_allocation()
+        self.width = rect.width
+        self.height = rect.height
+
+        self.xid = self.ventana.get_property('window').xid
+        self.player = PlayerBin(self.xid, self.width, self.height)
+
+        self.ventana.connect("expose-event", self.__set_size)
+
+    def __set_size(self, widget, event):
+        rect = self.ventana.get_allocation()
+        self.width = rect.width
+        self.height = rect.height
+
+        rot = self.player.get_rotacion()
+        if self.src_path:
+            self.load(self.src_path)
+
+        self.player.force_rotation(rot)
+
+    def load(self, uri):
+        self.src_path = uri
+
+        if self.player:
+            self.player.stop()
+            del(self.player)
+            self.player = False
+
+        self.player = PlayerBin(self.xid, self.width, self.height)
+        self.player.load(self.src_path)
+
+    def rotar(self, valor):
+        self.player.rotar(valor)
+
+    def stop(self):
+        self.player.stop()
+        try:
+            self.ventana.disconnect_by_func(self.__set_size)
+        except:
+            pass
+
+
+class PlayerBin(gobject.GObject):
+
+    def __init__(self, ventana_id, width, height):
+
+        gobject.GObject.__init__(self)
+
+        self.ventana_id = ventana_id
+        self.player = None
+        self.bus = None
+
+        self.player = gst.element_factory_make("playbin2", "player")
+        self.video_bin = Video_Out(width, height)
+        self.player.set_property('video-sink', self.video_bin)
+
+        self.bus = self.player.get_bus()
+        #self.bus.set_sync_handler(self.__bus_handler)
+        self.bus.add_signal_watch()                             # ****
+        #self.bus.connect('message', self.__on_mensaje)          # ****
+        self.bus.enable_sync_message_emission()                 # ****
+        self.bus.connect('sync-message', self.__sync_message)   # ****
+
+    def __sync_message(self, bus, message):
+        if message.type == gst.MESSAGE_ELEMENT:
+            if message.structure.get_name() == 'prepare-xwindow-id':
+                gtk.gdk.threads_enter()
+                gtk.gdk.display_get_default().sync()
+                message.src.set_xwindow_id(self.ventana_id)
+                gtk.gdk.threads_leave()
+
+        elif message.type == gst.MESSAGE_ERROR:
+            err, debug = message.parse_error()
+            if PR:
+                print "ImagePlayer ERROR:"
+                print "\t%s" % err
+                print "\t%s" % debug
+
+    '''
+    def __bus_handler(self, bus, message):
+        if message.type == gst.MESSAGE_ELEMENT:
+            if message.structure.get_name() == 'prepare-xwindow-id':
+                message.src.set_xwindow_id(self.ventana_id)
+
+        elif message.type == gst.MESSAGE_ERROR:
+            err, debug = message.parse_error()
+            if PR:
+                print "ImagePlayer ERROR:"
+                print "\t%s" % err
+                print "\t%s" % debug
+
+        return gst.BUS_PASS
+    '''
+
+    def __play(self):
+        self.player.set_state(gst.STATE_PLAYING)
+
+    def rotar(self, valor):
+        self.stop()
+        self.video_bin.rotar(valor)
+        self.__play()
+
+    def get_rotacion(self):
+        return self.video_bin.get_rotacion()
+
+    def force_rotation(self, rot):
+        self.video_bin.force_rotation(rot)
+
+    def stop(self):
+        self.player.set_state(gst.STATE_NULL)
+
+    def load(self, uri):
+        self.stop()
+        if not uri:
+            return
+
+        if os.path.exists(uri):
+            direccion = "file://" + uri
+            self.player.set_property("uri", direccion)
+            self.__play()
+
+        return False
+
+
+class Video_Out(gst.Pipeline):
+
+    def __init__(self, width, height):
+
+        gst.Pipeline.__init__(self)
+
+        self.set_name('video_out')
+
+        imagefreeze = gst.element_factory_make('imagefreeze', "imagefreeze")
+        videoconvert = gst.element_factory_make(
+            'ffmpegcolorspace', 'ffmpegcolorspace')
+
+        videoflip = gst.element_factory_make('videoflip', "videoflip")
+        caps = gst.Caps(
+            'video/x-raw-rgb,framerate=30/1,width=%s,height=%s' % (
+            width,height))
+        filtro = gst.element_factory_make("capsfilter", "filtro")
+        filtro.set_property("caps", caps)
+
+        ximagesink = gst.element_factory_make('ximagesink', "ximagesink")
+        ximagesink.set_property("force-aspect-ratio", True)
+
+        self.add(imagefreeze)
+        self.add(videoconvert)
+        self.add(videoflip)
+        self.add(filtro)
+        self.add(ximagesink)
+
+        imagefreeze.link(videoconvert)
+        videoconvert.link(videoflip)
+        videoflip.link(filtro)
+        filtro.link(ximagesink)
+
+        self.ghost_pad = gst.GhostPad(
+            "sink", imagefreeze.get_static_pad("sink"))
+
+        self.ghost_pad.set_target(imagefreeze.get_static_pad("sink"))
+
+        self.add_pad(self.ghost_pad)
+
+    def rotar(self, valor):
+        videoflip = self.get_by_name("videoflip")
+        rot = videoflip.get_property('method')
+        if valor == "Derecha":
+            if rot < 3:
+                rot += 1
+
+            else:
+                rot = 0
+
+        elif valor == "Izquierda":
+            if rot > 0:
+                rot -= 1
+
+            else:
+                rot = 3
+
+        videoflip.set_property('method', rot)
+
+    def force_rotation(self, rot):
+        videoflip = self.get_by_name("videoflip")
+        videoflip.set_property('method', rot)
+
+    def get_rotacion(self):
+        videoflip = self.get_by_name("videoflip")
+        return videoflip.get_property('method')
