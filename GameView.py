00fbff799ece70ae85e186ba52c918f48be487ae
Sebastian Silva <sebastian@somosazucar.org>
2014-12-05 17:33:46 -0500

    Cambiamos a subprocess....

--- GameView.py
+++ GameView.py
@@ -26,13 +26,7 @@ import gtk
 
 from Globales import COLORES
 
-import gobject
-import sugargame2
-import sugargame2.canvas
-import spyral
-import pygame
-from Games.ug1.runme import Escena
-
+import subprocess
 
 class GameView(gtk.EventBox):
 
@@ -43,56 +37,12 @@ class GameView(gtk.EventBox):
         self.modify_bg(gtk.STATE_NORMAL, COLORES["toolbar"])
         self.set_border_width(4)
 
-        self.game = False
-        self.pump = False
-
-        self.pygamecanvas = sugargame2.canvas.PygameCanvas(self)
-        self.pygamecanvas.set_flags(gtk.EXPAND)
-        self.pygamecanvas.set_flags(gtk.FILL)
-
-        self.pygamecanvas.set_events(gtk.gdk.BUTTON_PRESS_MASK)
-        self.pygamecanvas.connect(
-            "button-press-event", self.pygamecanvas.grab_focus)
-
-        self.add(self.pygamecanvas)
-
-        self.connect("size-allocate", self.__reescalar)
+        self.add(gtk.Label("Juego 1"))
         self.show_all()
 
-    def __reescalar(self, widget, event):
-        if self.game:
-            rect = self.get_allocation()
-            print "FIXME: El juego debe reescalarse a", rect.width, rect.height
-
-    def __run_game(self):
-        rect = self.get_allocation()
-        spyral.director.init((rect.width, rect.height),
-            fullscreen=False, max_fps=30)
-        self.game = Escena(self)
-        spyral.director.push(self.game)
-        if self.pump:
-            gobject.source_remove(self.pump)
-            self.pump = False
-        self.pump = gobject.timeout_add(300, self.__pump)
-        spyral.director.run(sugar=True)
-
-    def __pump(self):
-        # FIXME: HACK porque sino pygame acumula demasiados eventos.
-        # No es mejor hacer pygame.event.clear() ?
-        pygame.event.pump()
-        return True
-
     def stop(self):
-        print "FIXME: El juego debe detenerse y eliminarse."
-        if self.pump:
-            gobject.source_remove(self.pump)
-            self.pump = False
-        if self.game:
-            del(self.game)
-            self.game = False
         self.hide()
 
     def run(self, topic):
-        self.pygamecanvas.run_pygame(self.__run_game)
-        self.pygamecanvas.grab_focus()
+        subprocess.Popen("python2 Games/ug1/runme.py", shell=True)
         self.show()
