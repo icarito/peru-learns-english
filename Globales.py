b5ad1ee9d0ff5479b23d753e25a5f608458f243a
Sebastian Silva <sebastian@somosazucar.org>
2014-12-04 23:18:33 -0500

    Simplificar y eliminar dependencia

--- Globales.py
+++ Globales.py
@@ -21,7 +21,6 @@
 
 import commands
 import csv
-import chardet
 import gtk
 from gtk import gdk
 
@@ -34,15 +33,8 @@ COLORES = {
 
 
 def get_vocabulario(csvfile):
-    item = []
-    archivo = open(csvfile, 'rb')
-    encoding = chardet.detect(archivo.read())['encoding']
-    archivo.seek(0)
-    reader = csv.reader(archivo, dialect='excel', delimiter=',')
-    for index, row in enumerate(reader):
-        row = [x.decode(encoding) for x in row]
-        item.append(row)
-    return item
+    reader = csv.reader(file(csvfile))
+    return list(reader)
 
 
 def decir(pitch, speed, word_gap, voice, text):
